accb5525d9058a2f79f9c0cea83294267154eb32
Aya <ayahamedabdulaziz@gmail.com>
2021-04-25 00:47:25 +0200

    Broadcast messages all incoming messages.

--- chat-back/index.js
+++ chat-back/index.js
@@ -1,6 +1,6 @@
-const cors = require('cors');
-const express = require('express');
-const http = require('http');
+const cors = require("cors");
+const express = require("express");
+const http = require("http");
 
 const app = express();
 app.use(cors());
@@ -8,8 +8,20 @@ app.use(express.json());
 
 const server = http.createServer(app);
 
-const io = require('socket.io')(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methode: "*",
+  },
+});
 
-io.on('connection', (socket) => {
-    console.log('new connection', socket.id);
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+  socket.on("message", (data) => {
+    socket.broadcast.emit("message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Sever listening on port 3001");
 });
